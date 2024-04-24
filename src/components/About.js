9ca518a87276ced225491f35e46ff7df26d4dc82
amanikitutocd.. <akituto@gmail.com>
2024-04-24 07:27:38 +0300

    completed lab

--- src/components/About.js
+++ src/components/About.js
@@ -2,7 +2,17 @@ import React from "react";
 import { image } from "../data/data";
 
 function About() {
-  return <div>About</div>;
+  return (
+    <div id="about">
+      <h2>About Me</h2>
+      <p>
+        I am a software engineer with a passion for building user-friendly and
+        efficient applications. I am proficient in a variety of programming
+        languages and technologies, and I am always eager to learn new things.
+      </p>
+      <img src={image} alt="I made this" />
+    </div>
+  );
 }
 
 export default About;
